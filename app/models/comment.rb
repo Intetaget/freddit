534fa54937d68d2c2d5d3da588a88c8b4d02dc65
Joe Winter <joe@joeventures.com>
2015-10-31 12:25:47 -0400

    Updated comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :links
   belongs_to :user
 
-#validates_presence_of :content, :post_id  TITLES CONTENT AND POSTID WILL NEED TO BE CHANGED
+  validates_presence_of :content, :post_id
 
 end
